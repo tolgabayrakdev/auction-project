081b35f12625dbc3084525a8d9532ce1b4aa368b
tolgabayrakdev <bayraktolga28@gmail.com>
2023-07-26 11:58:09 +0300

    chaneg password

--- client/src/app/auth/change-password/[slug]/page.tsx
+++ client/src/app/auth/change-password/[slug]/page.tsx
@@ -1,15 +1,19 @@
 'use client';
 
-import { Card, PasswordInput, Button, Group, Box } from '@mantine/core';
+import { Card, PasswordInput, Button, Group, Alert } from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { notifications } from '@mantine/notifications';
+import { IconAlertCircle } from '@tabler/icons-react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 
 export default function page({ params }: { params: { slug: string } }) {
+  const router = useRouter();
   const form = useForm({
     initialValues: {
-      password: 'secret',
-      confirmPassword: 'sevret',
+      password: '',
+      confirmPassword: '',
     },
 
     validate: {
@@ -19,10 +23,31 @@ export default function page({ params }: { params: { slug: string } }) {
   });
   const [isOkey, setIsOkey] = useState(false);
   const [isExpired, setIsExpired] = useState(false);
+  const [isChanged, setIsChanged] = useState(false);
 
 
-  const handleChangePassword = async () => {
-    const result = await fetch("http://localhost:8001/api/v1/auth/")
+  const handleChangePassword = async (values: any) => {
+    console.log(values);
+    const result = await fetch(`http://localhost:8001/api/v1/auth/change-password/${params.slug}`, {
+      method: "PUT",
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ password: form.values.password }),
+
+    })
+    if (result.status === 200) {
+      setIsChanged(true);
+      setTimeout(() => {
+        router.push("/auth")
+      }, 1500)
+    } else {
+      notifications.show({
+        color: "yellow",
+        title: 'Ups',
+        message: 'Something going wrong!',
+      })
+    }
   }
 
   useEffect(() => {
@@ -36,6 +61,9 @@ export default function page({ params }: { params: { slug: string } }) {
       );
       if (result.status === 200) {
         setIsOkey(true);
+        setTimeout(() => {
+
+        }, 1500)
       } else if (result.status === 400) {
         setIsExpired(true);
       }
@@ -48,30 +76,37 @@ export default function page({ params }: { params: { slug: string } }) {
       {
         isOkey ?
           <div className='flex justify-center'>
-            <Card shadow="sm" padding="lg" radius="md" withBorder className=' w-2/5 mt-3'>
-              <p className='text-center mt-1'>Change password.</p>
-              <form onSubmit={form.onSubmit((values) => console.log(values))}>
-                <PasswordInput
-                  label="Password"
-                  placeholder="Password"
-                  {...form.getInputProps('password')}
-                />
+            {
+              isChanged ? <Alert className=' w-52 mt-1 rounded-sm' icon={<IconAlertCircle size="1rem" />} title="Successful" color="lime">
+                Password changed.
+              </Alert> :
+                <Card shadow="sm" padding="lg" radius="md" withBorder className=' w-2/5 mt-3'>
+                  <p className='text-center mt-1'>Change password.</p>
+                  <form onSubmit={form.onSubmit((values) => handleChangePassword(values))}>
+                    <PasswordInput
+                      label="Password"
+                      placeholder="Password"
+                      {...form.getInputProps('password')}
+                    />
 
-                <PasswordInput
-                  mt="sm"
-                  label="Confirm password"
-                  placeholder="Confirm password"
-                  {...form.getInputProps('confirmPassword')}
-                />
+                    <PasswordInput
+                      mt="sm"
+                      label="Confirm password"
+                      placeholder="Confirm password"
+                      {...form.getInputProps('confirmPassword')}
+                    />
 
-                <Group position="right" mt="md">
-                  <Button type="submit" variant="default">Submit</Button>
-                </Group>
-              </form>
-              <Link className='mr-3 hover:underline text-center' href="/auth"> Go to auth page</Link>
+                    <Group position="right" mt="md">
+                      <Button type="submit" variant="default">Submit</Button>
+                    </Group>
+                  </form>
+                  <Link className='mr-3 hover:underline text-center' href="/auth"> Go to auth page</Link>
 
-            </Card>
-            
+                </Card>
+
+            }
+
+
           </div>
           :
           <Link className='mr-3 hover:underline text-center' href="/auth"> Go to auth page</Link>}
