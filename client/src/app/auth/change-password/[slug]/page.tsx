c45493c2854eb6a009b6cb1e9ed6a3429b6a9987
tolgabayrakdev <bayraktolga28@gmail.com>
2023-07-26 10:38:42 +0300

    change password page

--- client/src/app/auth/change-password/[slug]/page.tsx
+++ client/src/app/auth/change-password/[slug]/page.tsx
@@ -1,10 +1,29 @@
 'use client';
+
+import { Card, PasswordInput, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 export default function page({ params }: { params: { slug: string } }) {
+  const form = useForm({
+    initialValues: {
+      password: 'secret',
+      confirmPassword: 'sevret',
+    },
+
+    validate: {
+      confirmPassword: (value, values) =>
+        value !== values.password ? 'Passwords did not match' : null,
+    },
+  });
   const [isOkey, setIsOkey] = useState(false);
   const [isExpired, setIsExpired] = useState(false);
-  console.log(params.slug);
+
+
+  const handleChangePassword = async () => {
+    const result = await fetch("http://localhost:8001/api/v1/auth/")
+  }
 
   useEffect(() => {
     const checkResetToken = async () => {
@@ -25,10 +44,38 @@ export default function page({ params }: { params: { slug: string } }) {
   }, []);
 
   return (
-    <div>
-      Burası Dinamik
-      {isOkey && <p>Burdan şifre değiştir.</p>}
-      {isExpired && <p>Reset link has expired</p>}
+    <div className='flex justify-center flex-col'>
+      {
+        isOkey ?
+          <div className='flex justify-center'>
+            <Card shadow="sm" padding="lg" radius="md" withBorder className=' w-2/5 mt-3'>
+              <p className='text-center mt-1'>Change password.</p>
+              <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                <PasswordInput
+                  label="Password"
+                  placeholder="Password"
+                  {...form.getInputProps('password')}
+                />
+
+                <PasswordInput
+                  mt="sm"
+                  label="Confirm password"
+                  placeholder="Confirm password"
+                  {...form.getInputProps('confirmPassword')}
+                />
+
+                <Group position="right" mt="md">
+                  <Button type="submit" variant="default">Submit</Button>
+                </Group>
+              </form>
+              <Link className='mr-3 hover:underline text-center' href="/auth"> Go to auth page</Link>
+
+            </Card>
+            
+          </div>
+          :
+          <Link className='mr-3 hover:underline text-center' href="/auth"> Go to auth page</Link>}
+      {isExpired && <p className='text-center'>Reset link has expired</p>}
     </div>
   );
 }
